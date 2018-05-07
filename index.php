18fdf940eedf0c17ebb808cee7c1f4811f3d2341
laiamolina <laiamolina97@gmail.com>
2018-05-07 20:59:28 +0200

    preparació login

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
- echo 'prova';
+
+    $routes = [
+        "/ola" => "ola.php",
+        "/adeu" => "adeu.php",
+        "/login" => "views/login.php"
+    ];
+
+    require($routes[$_SERVER["REQUEST_URI"]]);
 ?>
